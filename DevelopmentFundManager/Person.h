37ae807b70742290e19c3aeee4c68b3ba23100f1
tu1014 <tu1014@naver.com>
2021-11-12 15:27:50 +0900

    create Person class

---
+++ DevelopmentFundManager/Person.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "string"
+using namespace std;
+
+class Person
+{
+
+public:
+
+	Person();
+	~Person();
+
+
+private:
+
+	string name;
+	string phoneNumber;
+	int fundAmount;
+
+
+
+};
+
