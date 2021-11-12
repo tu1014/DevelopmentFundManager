3949329afd874e8ee110b827beb32feb4fd39383
tu1014 <tu1014@naver.com>
2021-11-12 17:13:32 +0900

    create Validator class

---
+++ DevelopmentFundManager/Validator.cpp
@@ -0,0 +1,17 @@
+#include "Validator.h"
+#include <string>
+using namespace std;
+
+
+bool Validator::isValidFundAmount(int amount) {
+
+	if (amount > 0) return true;
+	else return false;
+
+}
+
+bool Validator::isValidPhoneNumber(string phoneNumber) {
+
+
+
+}
---
+++ DevelopmentFundManager/Validator.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class Validator
+{
+
+public:
+
+	bool static isValidPhoneNumber(string phoneNumber);
+	bool static isValidFundAmount(int amount);
+
+
+};
+
