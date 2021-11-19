63f24187d4ee581179f17239250882a2f985990c
tu1014 <tu1014@naver.com>
2021-11-19 11:51:17 +0900

    컴파일 오류 수정

--- DevelopmentFundManager/MyList.h
+++ DevelopmentFundManager/MyList.h
@@ -1,21 +1,31 @@
 #pragma once
 using namespace std;
 
+
+template < typename T >
+class MyList;
+
 template<typename T>
+class Node;
+
+template < typename T >
 class MyList
 {
 
 public:
 
 	MyList();
-	~MyList();
+	// ~MyList();
+
+	bool insert(const T& ptr_item);
+	void print() const;
 
 
 
 
 private:
 
-	Node dummyHead;
+	Node<T> dummyHead;
 	int size;
 
 
@@ -27,22 +37,23 @@ class Node {
 
 public:
 
-	Node(T& item);
+	Node(T& ptr_item);
 	Node();
 	~Node();
 
-	const Node& getNext() const;
-	const Node& getPrev() const;
-	void setNext(Node& nextNode);
-	void setPrev(Node& prevNode);
+	const Node<T>& getNext() const;
+	const Node<T>& getPrev() const;
+	void setNext(Node<T>& nextNode);
+	void setPrev(Node<T>& prevNode);
 
+	const T& getItem() const;
 
 private:
 
 	T* ptr_item;
 
-	Node* ptr_prev;
-	Node* ptr_next;
+	Node<T>* ptr_prev;
+	Node<T>* ptr_next;
 
 	// 굳이 선언해야 할까?
 	friend class MyList<T>;
--- DevelopmentFundManager/MyList.hpp
+++ DevelopmentFundManager/MyList.hpp
@@ -6,7 +6,7 @@ template<typename T>
 Node<T>::Node() {
 
 	// circular dummy head constructor
-	ptr_item = null;
+	ptr_item = NULL;
 	ptr_prev = this;
 	ptr_next = this;
 
@@ -16,8 +16,8 @@ template<typename T>
 Node<T>::Node(T& element) {
 
 	ptr_item = element;
-	ptr_prev = null;
-	ptr_next = null;
+	ptr_prev = NULL;
+	ptr_next = NULL;
 
 }
 
@@ -42,8 +42,8 @@ const Node<T>& Node<T>::getPrev() const { return ptr_prev; }
 template<typename T>
 void Node<T>::setNext(Node& nextNode) {
 
-	nextNode.setPrev(this);
-	nextNode.setNext(ptr_next);
+	(*nextNode).setPrev(this);
+	(*nextNode).setNext(ptr_next);
 	ptr_next = nextNode;
 
 }
@@ -51,11 +51,14 @@ void Node<T>::setNext(Node& nextNode) {
 template<typename T>
 void Node<T>::setPrev(Node& prevNode) {
 
-	prevNode.setPrev(ptr_prev);
-	prevNode.setNext(this);
+	(*prevNode).setPrev(ptr_prev);
+	(*prevNode).setNext(this);
 	ptr_prev = prevNode;
 
 }
+
+template<typename T>
+const T& Node<T>::getItem() const { return ptr_item; }
 
 
 ////////////////////////////////////////////////////////////
@@ -63,6 +66,81 @@ void Node<T>::setPrev(Node& prevNode) {
 template<typename T>
 MyList<T>::MyList() {
 
+	size = 0;
+
+}
+
+template<typename T>
+bool MyList<T>::insert(const T& newItem) {
+
+	// 비어 있으면 맨 앞에 삽입
+
+	// 처음부터 끝까지 돌면서 중복 체크
+	// 루프 돌면서 삽입해야 할 인덱스 기억
+	// 루프 돌다 중복 발견하면 false 리턴
+
+	Node<T>* newNode = new Node<T>(newItem);
+
+	if (size == 0) {
+
+		size++;
+		(*newNode).setPrev(dummyHead);
+		(*newNode).setNext(dummyHead);
+		dummyHead.setPrev(newNode);
+		dummyHead.setNext(newNode);
+		cout << "삽입 성공" << endl;
+		return true;
+
+	}
+
+	Node<T>* tmpNode = dummyHead;
+	Node<T>* insertIndex = dummyHead.getNext();
+	T* item;
+	for (int i = 0; i < size; i++) {
+
+		tmpNode = (*tmpNode).getNext();
+		item = (*tmpNode).getItem();
+
+		if (newItem == *item) {
+
+			cout << "중복 발생" << endl;
+			delete newNode;
+			return false;
+
+		}
+
+		if ((*item) < (newItem)) insertIndex = tmpNode;
+
+	}
+
+	(*newNode).setPrev(insertIndex);
+	(*newNode).setNext((*insertIndex).getNext());
+	(*(*insertIndex).getNext()).setPrev(newNode);
+	(*insertIndex).setNext(newNode);
+
+	cout << "삽입 성공" << endl;
+	size++;
+
+	return true;
+
+}
+
+template<typename T>
+void MyList<T>::print() const {
+
+	cout << "출력" << endl;
+
+	if (size == 0) return;
+
+	Node<T>* node = dummyHead.getNext();
+	for (int i = 0; i < size; i++) {
+
+		cout << (*(*node).getItem) << endl;
+		node = (*node).getNext();
+
+	}
+
+	cout << "출력 끝" << endl;
 
 
 }
